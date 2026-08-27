029e99274a6c2420c3e3a2de903912c20ad72783
samiul2102013 <samiulhasan0186@gmail.com>
2026-08-27 13:27:37 +0600

    Set Arabic as default locale

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,6 +2,6 @@ import { defineRouting } from 'next-intl/routing';
 
 export const routing = defineRouting({
   locales: ['en', 'ar'],
-  defaultLocale: 'en',
+  defaultLocale: 'ar',
   localePrefix: 'as-needed',
 });
